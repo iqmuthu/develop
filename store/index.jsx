74c744c9f3f1229148996543019657b5cea65655
iqmuthu <37839380+iqmuthu@users.noreply.github.com>
2018-03-27 19:44:32 +0530

    Create index.jsx

---
+++ store/index.jsx
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import appReducers from './reducers/index.jsx';
+const createStoreWithInitialState = (initState) => {
+	return createStore(appReducers, initState, applyMiddleware(thunk));
+};
+
+export default createStoreWithInitialState;
