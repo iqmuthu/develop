03c6c5c944e594b224211e2c2906f3ce1e7c7f4c
iqmuthu <37839380+iqmuthu@users.noreply.github.com>
2018-04-02 16:12:19 +0530

    Delete index.jsx

--- index.jsx
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import rootName from "./rootName";
-import studentDetails from "./studentReducer";
-export default combineReducers({
-    rootName,
-    studentDetails
-});
